d7f695873a60df4b0e8e81ea3f68380d081e330a
Minshik Sohn <surajang@users.noreply.github.com>
2015-12-22 21:58:13 +0900

    Create background.js

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  chrome.app.window.create(
+    'index.html',
+    {
+      id: 'mainWindow',
+      bounds: {width: 800, height: 600}
+      /* createWindowOptions: {resizable: false}*/
+    }
+  );
+});
